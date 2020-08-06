6f3d6da9dd7644798c93542f8b6fd041f4cf26ed
nazaninj72 <nazaninjafary@gmail.com>
2020-08-05 23:16:26 -0400

    added  covid-bert arguments

--- models/main.py
+++ models/main.py
@@ -14,14 +14,14 @@ import time
 import torch.nn as nn
 from sklearn.metrics import confusion_matrix, classification_report
 from functions import *
-<<<<<<< HEAD
 import argparse
 import torch
-=======
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
-
-# from args import parse_args
-# args = parser.parse_args()
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('output.log')
+fh.setLevel(logging.DEBUG) # or any level you want
+logger.addHandler(fh)
 
 
 parser = argparse.ArgumentParser()
@@ -31,11 +31,17 @@ parser.add_argument('--random_seed', type=int, default=42, help='random_seed [de
 parser.add_argument('--batch_size', type=int, default=32, help='batch size for training [default: 32]')
 # parser.add_argument('-hidden-dim', type=int, default=768, help='number of hidden dimension [default: 768]')
 ## view
-parser.add_argument('--classifier_type', type=str, default='bert', help='type of classification [default: bert]')
+parser.add_argument('--with_metadata',action="store_true", help='Whether to use metadata features')
 parser.add_argument('--learningrate', type=float, default=5e-5, help='learning rate [default: 5e-5]')
 # parser.add_argument('-','-max_len', type=int, default=160, help='maximum length of sentences [default: 160]')
 # parser.add_argument('-reply_sample_frequency', type=int, default=1, help='1:without sample')
-
+parser.add_argument(
+        '--model',
+        type=str,
+        default='bert',
+        choices=['bert', 'ct-bert'],
+        help='Type of models [default: bert]'
+    )
 args = parser.parse_args()
 
 
@@ -44,8 +50,8 @@ args = parser.parse_args()
 
 if torch.cuda.is_available():       
     device = torch.device("cuda")
-    print(f'There are {torch.cuda.device_count()} GPU(s) available.')
-    print('Device name:', torch.cuda.get_device_name(0))
+    # print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+    # print('Device name:', torch.cuda.get_device_name(0))
 
 else:
     print('No GPU available, using the CPU instead.')
@@ -68,11 +74,7 @@ def set_seed(seed_value=42):
     torch.manual_seed(seed_value)
     torch.cuda.manual_seed_all(seed_value)
 
-<<<<<<< HEAD
-def train(model, train_dataloader, val_dataloader=None, epochs=4,classifier_type='bert',evaluation=False):
-=======
-def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False,classifier_type='bert'):
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
+def train(model, train_dataloader, val_dataloader=None, epochs=4,with_metadata=False,evaluation=False):
     """Train the BertClassifier model.
     """
     # Start training loop
@@ -100,15 +102,20 @@ def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=Fal
             # Load batch to GPU
             # Zero out any previously calculated gradients
             model.zero_grad()
-            if classifier_type=='bert': 
+            if with_metadata: 
+                b_input_ids, b_attn_mask,b_md, b_labels = tuple(t.to(device) for t in batch)
+                # Perform a forward pass. This will return logits.
+                logits = model(b_input_ids, b_attn_mask,b_md)
+                
+
+            else: 
                 b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
                 # Perform a forward pass. This will return logits.
                 logits = model(b_input_ids, b_attn_mask)
 
-            elif classifier_type == 'bert_metadata':
-                b_input_ids, b_attn_mask,b_md, b_labels = tuple(t.to(device) for t in batch)
-                # Perform a forward pass. This will return logits.
-                logits = model(b_input_ids, b_attn_mask,b_md)
+
+
+                
 
             # Compute loss and accumulate the loss values
             loss = loss_fn(logits, b_labels)
@@ -147,8 +154,8 @@ def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=Fal
         if evaluation == True:
             # After the completion of each training epoch, measure the model's performance
             # on our validation set.
-            val_loss, val_accuracy,_ = evaluate(model, val_dataloader)
-            _,train_accuracy,_=evaluate(model,train_dataloader)
+            val_loss, val_accuracy,_ = evaluate(model, val_dataloader,with_metadata)
+            _,train_accuracy,_=evaluate(model,train_dataloader,with_metadata)
             # Print performance over the entire training data
             time_elapsed = time.time() - t0_epoch
             
@@ -159,7 +166,7 @@ def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=Fal
     print("Training complete!")
 
 
-def evaluate(model, val_dataloader,classifier_type='bert'):
+def evaluate(model, val_dataloader,with_metadata=False):
     """After the completion of each training epoch, measure the model's performance
     on our validation set.
     """
@@ -174,39 +181,26 @@ def evaluate(model, val_dataloader,classifier_type='bert'):
     # For each batch in our validation set...
     for batch in val_dataloader:
         # Load batch to GPU
-        if classifier_type=='bert': 
+        if with_metadata: 
+            b_input_ids, b_attn_mask,b_md, b_labels = tuple(t.to(device) for t in batch)
+            # Perform a forward pass. This will return logits.
+            logits = model(b_input_ids, b_attn_mask,b_md)
+            # Compute logits
+            with torch.no_grad():
+                logits = model(b_input_ids, b_attn_mask,b_md)
+            
+
+        else:
             b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
-<<<<<<< HEAD
 
             # Compute logits
             with torch.no_grad():
                 logits = model(b_input_ids, b_attn_mask)
 
-        elif classifier_type == 'bert_metadata':
-            b_input_ids, b_attn_mask,b_md, b_labels = tuple(t.to(device) for t in batch)
-            # Perform a forward pass. This will return logits.
-            logits = model(b_input_ids, b_attn_mask,b_md)
-            # Compute logits
-            with torch.no_grad():
-                logits = model(b_input_ids, b_attn_mask,b_md)
+
+            
 
 
-=======
-
-            # Compute logits
-            with torch.no_grad():
-                logits = model(b_input_ids, b_attn_mask)
-
-        elif classifier_type == 'bert_metadata':
-            b_input_ids, b_attn_mask,b_md, b_labels = tuple(t.to(device) for t in batch)
-            # Perform a forward pass. This will return logits.
-            logits = model(b_input_ids, b_attn_mask,b_md)
-            # Compute logits
-            with torch.no_grad():
-                logits = model(b_input_ids, b_attn_mask,b_md)
-
-
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
         
 
         # Compute loss
@@ -233,41 +227,27 @@ def evaluate(model, val_dataloader,classifier_type='bert'):
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     MAX_LENGTH=160
 
     epochs=args.epoch
     batch_size=args.batch_size
-   
+    model_type=args.model
     learningrate=args.learningrate
-    classifier_type = args.classifier_type
-=======
-    # args = parse_args(argv)
-    epochs=20
-    learningrate=5e-5
-    classifier_type = 'bert_metadata'
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
+    with_metadata = args.with_metadata
+    print("Experiment details:  #epochs "+str(epochs)+" batch size "+str(batch_size)+" learning rate  "+str(learningrate)+" model "+model_type+" with metadata "+str(with_metadata))
     X = data.tweet.values
     y = data.label.values
     random_state=args.random_seed
 
     indices = np.arange(len(X))
-<<<<<<< HEAD
-    train_idx, val_idx, y_train, y_val= train_test_split(indices, y,stratify = y, test_size=0.1, random_state=random_state)
-=======
-    train_idx, val_idx, y_train, y_val= train_test_split(indices, y,stratify = y, test_size=0.1, random_state=42)
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
+
+    train_idx, val_idx, y_train, y_val= train_test_split(indices, y,stratify = y, test_size=0.2, random_state=random_state)
     X_train = X[train_idx]
     X_val = X[val_idx]
     
 
-<<<<<<< HEAD
     train_inputs, train_masks = preprocessing_for_bert(X_train,MAX_LENGTH)
     val_inputs, val_masks = preprocessing_for_bert(X_val,MAX_LENGTH)
-=======
-    train_inputs, train_masks = preprocessing_for_bert(X_train,max_length)
-    val_inputs, val_masks = preprocessing_for_bert(X_val,max_length)
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
 
     
 
@@ -276,16 +256,7 @@ if __name__ == "__main__":
     val_labels = torch.tensor(y_val)
 
     # Create the DataLoader for our training set
-    if classifier_type =='bert':
-        train_data = TensorDataset(train_inputs, train_masks, train_labels)
-        train_sampler = RandomSampler(train_data)
-        train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-
-        # Create the DataLoader for our validation set
-        val_data = TensorDataset(val_inputs, val_masks, val_labels)
-        val_sampler = SequentialSampler(val_data)
-        val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
-    elif classifier_type=='bert_metadata':
+    if with_metadata:
         # Create the DataLoader for our training set
         md_X=get_metadata_features()
         mdX_train = md_X[train_idx]
@@ -301,17 +272,22 @@ if __name__ == "__main__":
         val_sampler = SequentialSampler(val_data)
         val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
 
+        
+    else:
+        train_data = TensorDataset(train_inputs, train_masks, train_labels)
+        train_sampler = RandomSampler(train_data)
+        train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+        # Create the DataLoader for our validation set
+        val_data = TensorDataset(val_inputs, val_masks, val_labels)
+        val_sampler = SequentialSampler(val_data)
+        val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+        
 
     set_seed(42)    # Set seed for reproducibility
-<<<<<<< HEAD
-    ctbert_classifier, optimizer, scheduler = initialize_model(train_dataloader,epochs,learningrate,classifier_type)
-    train(ctbert_classifier, train_dataloader, val_dataloader, epochs,classifier_type,evaluation=True)
-    val_loss, val_accuracy,y_pred= evaluate(ctbert_classifier, val_dataloader,classifier_type)
-=======
-    ctbert_classifier, optimizer, scheduler = initialize_model(train_dataloader,epochs,learningrate,classifier_type='bert_metadata')
-    train(ctbert_classifier, train_dataloader, val_dataloader, epochs, evaluation=True,classifier_type='bert_metadata')
-    val_loss, val_accuracy,y_pred= evaluate(ctbert_classifier, val_dataloader,classifier_type='bert_metadata')
->>>>>>> 5b1037284257cab134e357c2fbcae24e2b2fb302
+    ctbert_classifier, optimizer, scheduler = initialize_model(train_dataloader,epochs,learningrate,with_metadata)
+    train(ctbert_classifier, train_dataloader, val_dataloader, epochs,with_metadata,evaluation=True)
+    val_loss, val_accuracy,y_pred= evaluate(ctbert_classifier, val_dataloader,with_metadata)
     np_preds=[]
     for i in y_pred:
         b=i.cpu().detach().numpy()
